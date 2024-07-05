bef2634df130774655967f7994f424324ed69220
Kevin Lee <klee.taurus@gmail.com>
2024-07-05 16:51:02 +0800

    Revert "remove contact us"
    This reverts commit 64eeaa2cd504e25490ea7b3d5d989a549aff4a09.

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -2,7 +2,7 @@
 // import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
-// import { CONTACT_US_EMAIL } from '@/lib/env';
+import { CONTACT_US_EMAIL } from '@/lib/env';
 
 /*
 function InfoLink({
@@ -65,6 +65,21 @@ export default function Footer() {
         <div className='flex flex-col items-center lg:items-stretch'>
           <h1 className='text-xl font-bold text-white lg:h-8 lg:text-[32px]'>{t('title')}</h1>
           <h2 className='text-xs'>{t('subTitle')}</h2>
+        </div>
+        <div className='mt-5 flex flex-col items-center gap-y-5 lg:mt-0 lg:flex-row lg:items-stretch lg:gap-x-10'>
+          <div className='grid grid-cols-2 gap-x-10 gap-y-5 lg:grid-cols-1 lg:gap-3'>
+            {/* INFO_LIST.map((item) => (
+              <InfoLink key={item.href} href={item.href} title={item.title} />
+            )) */}
+            <a
+              href={`mailto:${CONTACT_US_EMAIL}`}
+              className='whitespace-nowrap text-xs hover:opacity-70 lg:text-sm'
+              title={t('contactUs')}
+              type='email'
+            >
+              {t('contactUs')}
+            </a>
+          </div>
         </div>
       </div>
     </footer>
